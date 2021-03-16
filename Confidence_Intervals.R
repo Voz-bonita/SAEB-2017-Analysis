4dbadcc194ece7e6b28994f0303e54d9b35f4582
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-03-16 13:54:55 -0300

    Categorical data analysis

--- Confidence_Intervals.R
+++ Confidence_Intervals.R
@@ -5,64 +5,80 @@
 
 library(ggplot2)
 
-population <- read.csv("SAEB.sample.csv")
+# population <- read.csv("SAEB.sample.csv")
 
-CI <- function (data, confidence = 0.95, sd){
+CI <- function (data, confidence = 0.95, sd, variable = NULL){
 
   data <- as.data.frame(data)
+
   n <- length(data[,1])
-  sample_mean <- mean(data[,1])
 
   sup_quant <- (1-confidence)/2 + confidence
-  error <- qnorm(sup_quant) * sd/sqrt(n)
 
-  inf_lim <- sample_mean - error
-  sup_lim <- sample_mean + error
 
+  if (is.character(data[,1])){
+    names(data) <- c("Class")
+    data <- data %>%
+      group_by(Class) %>%
+      count()
+
+
+    sample.prop <- data$n[which(data$Class == variable)]/n
+    error <- qnorm(sup_quant) * sd
+
+    inf_lim <- sample.prop - error
+    sup_lim <- sample.prop + error
+
+  }else{
+    sample_mean <- mean(data[,1])
+    error <- qnorm(sup_quant) * sd/sqrt(n)
+
+    inf_lim <- sample_mean - error
+    sup_lim <- sample_mean + error
+
+
+  }
   returnValue(c(inf_lim,sup_lim))
 }
 
 n1 <- 30
+n2 <- 100
 max_samples <- 50
 
 samples_30 <- list()
+samples_100 <- list()
 
 set.seed(1234)
 
 for (i in 1:max_samples){
   sample_i <- population[sample(nrow(population), size = n1, replace = FALSE),]
   samples_30[[i]] <- sample_i
+
+  sample_j <- population[sample(nrow(population), size = n2, replace = FALSE),]
+  samples_100[[i]] <- sample_j
 }
 
-rm(`sample_i`)
 rm(`i`)
+rm(`sample_i`)
+rm(`sample_j`)
 
 
-mean_lp <- mean(population$NOTA_LP)
-var_lp <- var(population$NOTA_LP)
-sd_lp <- sqrt(var_lp)
-
-
-mean_mt <- mean(population$NOTA_MT)
-var_mt <- var(population$NOTA_MT)
-sd_mt <- sqrt(var_mt)
-
-
-
-math_df <- lp_df <- data.frame(matrix(ncol = 3, nrow = max_samples))
-math_df[,1] <- lp_df[,1] <- seq(1,max_samples)
+math_df <- lp_df <- local_df <- gender_df <- data.frame(matrix(ncol = 3, nrow = max_samples))
+math_df[,1] <- lp_df[,1] <- local_df[,1] <- gender_df[,1] <- seq(1,max_samples)
 
 
 category <- list()
-category[["NOTA_MT"]] <- list(math_df, mean_mt, var_mt, sd_mt)
-category[["NOTA_LP"]] <- list(lp_df, mean_lp, var_lp, sd_lp)
+category[["NOTA_MT"]] <- list(math_df, mean_mt, var_mt, sd_mt, "")
+category[["NOTA_LP"]] <- list(lp_df, mean_lp, var_lp, sd_lp, "")
+category[["SEXO"]] <- list(gender_df, gender.prop, gender.var, gender.sd, "Female")
+category[["LOCALIZACAO"]] <- list(local_df, local.prop, local.var, local.sd, "Rural")
 
-# Calculating category
+# Calculating Confidence Intervals for each sample
 for (name in names(category)){
-  names(category[[name]]) <- c("Interval_df", "Mean", "Var", "SD")
+  names(category[[name]]) <- c("Interval_df", "Mean", "Var", "SD", "Main_Variable")
 
   for (i in 1:max_samples){
-    Interval_i <- CI(samples_30[[i]][name], sd = category[[name]]$SD)
+    Interval_i <- CI(samples_30[[i]][name], sd = category[[name]]$SD, variable = category[[name]]$Main_Variable)
     category[[name]]$Interval_df[i,2:3] <- Interval_i
   }
 
@@ -73,4 +89,4 @@ for (name in names(category)){
 ggplot(data = category[[name]]$Interval_df, aes(x = category[[name]]$Mean,y = `Index`)) +
   geom_vline(xintercept=category[[name]]$Mean, linetype="dashed") +
   geom_errorbar(xmin = category[[name]]$Interval_df$Inf_lim, xmax = category[[name]]$Interval_df$Sup_lim) +
-  xlim(min(category[[name]]$Interval_df$Inf_lim)-5, max(category[[name]]$Interval_df$Sup_lim)+5)
+  xlim(min(category[[name]]$Interval_df$Inf_lim)-0.1, max(category[[name]]$Interval_df$Sup_lim)+0.1)
